4f5acc61d7f98cd536599f1552e781f511c66429
Camilla Leisti <camilla@91-159-59-232.elisa-laajakaista.fi>
2020-05-22 12:06:31 +0300

    color-codes

---
+++ src/colors.c
@@ -0,0 +1,34 @@
+void		red()
+{
+  printf("\033[1;31m");
+}
+
+void		green()
+{
+	printf("\033[1;32");
+}
+
+void		yellow()
+{
+  printf("\033[1;33m");
+}
+
+void		blue()
+{
+	printf("\033[1;34");
+}
+
+void		magenta()
+{
+	printf("\033[1;35");
+}
+
+void		cyan()
+{
+	printf("\033[1;36");
+}
+
+void		reset()
+{
+  printf("\033[0m");
+}
