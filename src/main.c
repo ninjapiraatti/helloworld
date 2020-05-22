37bde216368eb6cc04d71b76499904e061cf4753
Ninjapiraatti <tuoppil@gmail.com>
2020-05-22 16:15:01 +0300

    Added colors to input

--- src/main.c
+++ src/main.c
@@ -12,8 +12,10 @@ int     main(void)
     if(!(hello = malloc(sizeof(t_hello))))
         return (0);
     inithello(hello);
+    printf("\033[1;35m");
     printf("Enter number: ");
     scanf("%s", str1);
+    printf("\033[0m"); 
     num =  atoi(str1);
     hyphens(num);
     song(15);
